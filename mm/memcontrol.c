fed8064bf01ab720f1741bd7c7cb18554b1158cf
Sungwoo Park <swpark@nexell.co.kr>
2018-02-22 11:47:22 +0900

    mm: memcontrol: avoid unused function warning
    This patch fixes below compile warning.
    
    mm/memcontrol.c:5683:27: warning: 'mem_cgroup_id_get_online' defined but not
    used [-Wunused-function]
     static struct mem_cgroup *mem_cgroup_id_get_online(struct mem_cgroup *memcg)
    
    Change-Id: I0628fe21de0c9fd4a0becd6bb2ad7181d813ea5b
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5680,23 +5680,6 @@ static int __init mem_cgroup_init(void)
 subsys_initcall(mem_cgroup_init);
 
 #ifdef CONFIG_MEMCG_SWAP
-static struct mem_cgroup *mem_cgroup_id_get_online(struct mem_cgroup *memcg)
-{
-	while (!atomic_inc_not_zero(&memcg->id.ref)) {
-		/*
-		 * The root cgroup cannot be destroyed, so it's refcount must
-		 * always be >= 1.
-		 */
-		if (WARN_ON_ONCE(memcg == root_mem_cgroup)) {
-			VM_BUG_ON(1);
-			break;
-		}
-		memcg = parent_mem_cgroup(memcg);
-		if (!memcg)
-			memcg = root_mem_cgroup;
-	}
-	return memcg;
-}
 
 /**
  * mem_cgroup_swapout - transfer a memsw charge to swap
