ebc40620c4a4fba0eee4fb5f7e2b740eef26349a
Dongil Park <dongil01.park@samsung.com>
2018-02-22 11:19:39 +0900

    usb: host: ehci-exynos: Add missing reset_control_put
    This patch adds missing reset_control_put() to avoid memory leak.
    
    Change-Id: Iac4bbb2d064d754555f56a2ab29eed8aa7f5acd0
    Signed-off-by: Dongil Park <dongil01.park@samsung.com>

--- drivers/usb/host/ehci-exynos.c
+++ drivers/usb/host/ehci-exynos.c
@@ -109,10 +109,11 @@ static int exynos_ehci_phy_enable(struct device *dev)
 #ifdef CONFIG_RESET_CONTROLLER
 		struct reset_control *rst;
 
-		rst = devm_reset_control_get(dev, "usbhost-reset");
+		rst = reset_control_get(dev, "usbhost-reset");
 		if (!IS_ERR(rst)) {
 			if (reset_control_status(rst))
 				reset_control_reset(rst);
+			reset_control_put(rst);
 		}
 #endif
 	}
