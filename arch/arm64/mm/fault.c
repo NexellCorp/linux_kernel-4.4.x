4f49f2f07914c19bd657281ec09ed550f41835e3
Sungwoo Park <swpark@nexell.co.kr>
2018-02-22 21:18:49 +0900

    arm64: mm: avoid incomplete pointer type warning
    This patch fixes below compile warning
    
    arch/arm64/kernel/cpufeature.c:726:3: warning: initialization from incompatible
    pointer type
       .enable = cpu_enable_uao,
       ^
    arch/arm64/kernel/cpufeature.c:726:3: warning: (near initialization for
    'arm64_features[3].enable')
    
    Change-Id: I03d90708f39555cb143beec60d07330d29befd36
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -648,9 +648,8 @@ void cpu_enable_pan(void *__unused)
  * We need to enable the feature at runtime (instead of adding it to
  * PSR_MODE_EL1h) as the feature may not be implemented by the cpu.
  */
-int cpu_enable_uao(void *__unused)
+void cpu_enable_uao(void *__unused)
 {
 	asm(SET_PSTATE_UAO(1));
-	return 0;
 }
 #endif /* CONFIG_ARM64_UAO */
