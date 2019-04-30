a032182c0e8f53061bd6f399306a2fcda345f486
HyejungKwon <cjscld15@nexell.co.kr>
2019-04-30 13:29:38 +0900

    usb: dwc2: Modify a function 'sel_dr_mode_show'
    This patch modifies the function from checking current dr_mode
    to show current data role by checing current isr routine.
    
    Signed-off-by: HyejungKwon <cjscld15@nexell.co.kr>
    Change-Id: I10e6ad2dd923ef29b5d44f1abacc741c5c291131
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.4.x/+/452500
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -3331,16 +3331,12 @@ void dwc2_hcd_queue_transactions(struct dwc2_hsotg *hsotg,
 static ssize_t sel_dr_mode_show(struct device *dev,
 				   struct device_attribute *attr, char *buf)
 {
-	enum usb_dr_mode mode;
+	struct dwc2_hsotg *hsotg = dev_get_drvdata(dev);
 
-	mode = usb_get_dr_mode(dev);
-
-	if (mode == USB_DR_MODE_HOST)
+	if (dwc2_is_host_mode(hsotg))
 		return sprintf(buf, "%s", "host\n");
-	else if (mode == USB_DR_MODE_PERIPHERAL)
-		return sprintf(buf, "%s", "device\n");
 	else
-		return sprintf(buf, "%s", "otg\n");
+		return sprintf(buf, "%s", "device\n");
 }
 
 static ssize_t sel_dr_mode_store(struct device *dev,
