5fed46d1e81ac1a3d912ecd92280da471ec5096e
Hyunseok Jung <hsjung@nexell.co.kr>
2019-05-15 11:31:57 +0900

    usb: dwc2: Fix sysfs 'sel_dr_mode' return status value.
    This patch Fix 'sel_dr_mode' return status value
    
    $> echo host > /sys/class/udc/c0040000.dwc2otg/device/sel_dr_mode
    $> cat /sys/class/udc/c0040000.dwc2otg/device/sel_dr_mode
      : host
    
    $> echo device > /sys/class/udc/c0040000.dwc2otg/device/sel_dr_mode
    $> cat /sys/class/udc/c0040000.dwc2otg/device/sel_dr_mode
      : device
    
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>
    Change-Id: Id83f206abcec65c3b66908e229940c812792920b
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.4.x/+/454535
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.4.x/+/461115

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -3329,18 +3329,16 @@ void dwc2_hcd_queue_transactions(struct dwc2_hsotg *hsotg,
 }
 
 static ssize_t sel_dr_mode_show(struct device *dev,
-				   struct device_attribute *attr, char *buf)
+                                  struct device_attribute *attr, char *buf)
 {
-	enum usb_dr_mode mode;
+       struct dwc2_hsotg *hsotg = dev_get_drvdata(dev);
 
-	mode = usb_get_dr_mode(dev);
-
-	if (mode == USB_DR_MODE_HOST)
-		return sprintf(buf, "%s", "host\n");
-	else if (mode == USB_DR_MODE_PERIPHERAL)
-		return sprintf(buf, "%s", "device\n");
-	else
-		return sprintf(buf, "%s", "otg\n");
+       if (hsotg->dr_mode == USB_DR_MODE_HOST)
+               return sprintf(buf, "%s", "host\n");
+       else if (hsotg->dr_mode == USB_DR_MODE_PERIPHERAL)
+               return sprintf(buf, "%s", "device\n");
+       else
+               return sprintf(buf, "%s", "otg\n");
 }
 
 static ssize_t sel_dr_mode_store(struct device *dev,
