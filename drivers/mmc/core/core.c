1dcf93aa8e4fd781a9b528c3de7da3a74e6558ef
JongKeun Choi <jkchoi@nexell.co.kr>
2019-07-03 10:36:20 +0900

    ckboot6818: add code to reduce mmc delay time for quick boot
    This patch adds the code to reduce the mmc delay time for quick boot.
    
    Signed-off-by: JongKeun Choi <jkchoi@nexell.co.kr>
    Change-Id: I26c96b81865822f1c217d6bfcf3115e26a010d48

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1654,7 +1654,11 @@ int mmc_set_signal_voltage(struct mmc_host *host, int signal_voltage, u32 ocr)
 	}
 
 	/* Keep clock gated for at least 10 ms, though spec only says 5 ms */
+#ifndef CONFIG_MMC_INIT_LEVEL_UP
 	mmc_delay(10);
+#else
+	mmc_delay(1);
+#endif
 	host->ios.clock = clock;
 	mmc_set_ios(host);
 
@@ -1764,7 +1768,11 @@ void mmc_power_up(struct mmc_host *host, u32 ocr)
 	 * This delay should be sufficient to allow the power supply
 	 * to reach the minimum voltage.
 	 */
+#ifndef CONFIG_MMC_INIT_LEVEL_UP
 	mmc_delay(10);
+#else
+	mmc_delay(1);
+#endif
 
 	mmc_pwrseq_post_power_on(host);
 
@@ -1777,7 +1785,11 @@ void mmc_power_up(struct mmc_host *host, u32 ocr)
 	 * This delay must be at least 74 clock sizes, or 1 ms, or the
 	 * time required to reach a stable voltage.
 	 */
+#ifndef CONFIG_MMC_INIT_LEVEL_UP
 	mmc_delay(10);
+#else
+	mmc_delay(1);
+#endif
 }
 
 void mmc_power_off(struct mmc_host *host)
