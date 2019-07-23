691a596eafa51028d54e659d50a143a7f91af710
GyoungBo Min <mingyoungbo@nexell.co.kr>
2019-07-23 10:34:27 +0900

    nexell: Fix build error for module build
    This patch fixes build error when module build.
     - add EXPORT_SYMBOL_GPL
    
    Signed-off-by: GyoungBo Min <mingyoungbo@nexell.co.kr>
    Change-Id: Iecfbe6996716a9124f7164af3af0dfd79b0dd17e

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -1006,3 +1006,5 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 	dev->archdata.dma_coherent = coherent;
 	__iommu_setup_dma_ops(dev, dma_base, size, iommu);
 }
+
+EXPORT_SYMBOL_GPL(arch_setup_dma_ops);
