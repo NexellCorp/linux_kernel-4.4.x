66cb06f84501ebcc794447cead6b3b411222b13b
Lee Geunjeong <kjlee@nexell.co.kr>
2019-09-06 14:27:51 +0900

    net: wireless: bcmdhd_cypress: Disable wl debug all
    This patch disables the wl debug all.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: Ia945cb8868387a8789ca1e82f5ebb50253a3b05d

--- drivers/net/wireless/bcmdhd_cypress/wl_cfg80211.h
+++ drivers/net/wireless/bcmdhd_cypress/wl_cfg80211.h
@@ -2,15 +2,15 @@
  * Linux cfg80211 driver
  *
  * Portions of this code are copyright (c) 2017, Cypress Semiconductor Corporation
- * 
+ *
  * Copyright (C) 1999-2017, Broadcom Corporation
- * 
+ *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
  * following added to such license:
- * 
+ *
  *      As a special exception, the copyright holders of this software give you
  * permission to link this software with independent modules, and to copy and
  * distribute the resulting executable under terms of your choice, provided that
@@ -18,7 +18,7 @@
  * the license of that module.  An independent module is a module which is not
  * derived from this software.  The special exception does not apply to any
  * modifications of the software.
- * 
+ *
  *      Notwithstanding the above, under no circumstances may you combine this
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
@@ -65,7 +65,7 @@ struct wl_ibss;
 #define WL_DBG_ERR	(1 << 0)
 
 /* 0 invalidates all debug messages.  default is 1 */
-#define WL_DBG_LEVEL 0xFF
+#define WL_DBG_LEVEL 0
 
 #ifdef CUSTOMER_HW4
 #define CFG80211_ERROR_TEXT		"CFG80211-INFO2) "
@@ -672,7 +672,7 @@ struct bcm_cfg80211 {
 	bool scan_tried;	/* indicates if first scan attempted */
 #if defined(BCMSDIO)
 	bool wlfc_on;
-#endif 
+#endif
 	bool vsdb_mode;
 	bool roamoff_on_concurrent;
 	u8 *ioctl_buf;		/* ioctl buffer */
