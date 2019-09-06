0654b1c49ba05ffbc29b9ef71a89181098124859
Lee Geunjeong <kjlee@nexell.co.kr>
2019-09-06 14:27:51 +0900

    net: wireless: bcmdhd_cypress: Disable wl debug all
    This patch disables the wl debug all.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: Ia945cb8868387a8789ca1e82f5ebb50253a3b05d

--- drivers/net/wireless/bcmdhd_cypress/dhd_common.c
+++ drivers/net/wireless/bcmdhd_cypress/dhd_common.c
@@ -2,15 +2,15 @@
  * Broadcom Dongle Host Driver (DHD), common DHD core.
  *
  * Portions of this code are copyright (c) 2017, Cypress Semiconductor Corporation
- * 
+ *
  * Copyright (C) 1999-2017, Broadcom Corporation
- * 
+ *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
  * following added to such license:
- * 
+ *
  *      As a special exception, the copyright holders of this software give you
  * permission to link this software with independent modules, and to copy and
  * distribute the resulting executable under terms of your choice, provided that
@@ -18,7 +18,7 @@
  * the license of that module.  An independent module is a module which is not
  * derived from this software.  The special exception does not apply to any
  * modifications of the software.
- * 
+ *
  *      Notwithstanding the above, under no circumstances may you combine this
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
@@ -120,15 +120,15 @@ bool ap_fw_loaded = FALSE;
 #define DHD_COMPILED "\nCompiled in " SRCBASE
 #endif /* DHD_DEBUG */
 
+#if defined(DHD_DEBUG)
 #if defined(DHD_VERSION_NO_DATE_TIME)
 const char dhd_version[] = "Dongle Host Driver, version " EPI_VERSION_STR	DHD_COMPILED;
 #else
-#if defined(DHD_DEBUG)
 const char dhd_version[] = "Dongle Host Driver, version " EPI_VERSION_STR
 	DHD_COMPILED " on " __DATE__ " at " __TIME__;
+#endif
 #else
 const char dhd_version[] = "\nDongle Host Driver, version " EPI_VERSION_STR "\nCompiled from ";
-#endif 
 #endif /* DHD_VERSION_NO_DATE_TIME */
 
 void dhd_set_timer(void *bus, uint wdtick);
