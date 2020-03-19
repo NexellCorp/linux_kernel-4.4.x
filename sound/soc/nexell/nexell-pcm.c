b8a4b04b270d3462b28a6a19a5b84d5599425a12
Hyunseok Jung <hsjung@nexell.co.kr>
2020-03-19 14:08:42 +0900

    ASoC: nexell: initialize timestamp value when dma resume
    This patch initializes timestamp value when dma resume.
    it update time value for audio compensation.
    
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>
    Change-Id: Id5769a5034ab5431b9af3c8837fd9590386dad01

--- sound/soc/nexell/nexell-pcm.c
+++ sound/soc/nexell/nexell-pcm.c
@@ -387,6 +387,7 @@ static int nx_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 	case SNDRV_PCM_TRIGGER_RESUME:
 	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
 		dmaengine_resume(prtd->dma_chan);
+		prtd->time_stamp_us = ktime_to_us(ktime_get());
 		break;
 
 	case SNDRV_PCM_TRIGGER_SUSPEND:
